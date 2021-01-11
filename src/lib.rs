3aa6fe1c663e17c43b786598d9c3f65427579fd7
Rod Conan <rconan@gmto.org>
2021-01-10 22:39:26 -0800

    DELAUNAY AREA BUG FIX
    U

--- src/lib.rs
+++ src/lib.rs
@@ -145,7 +145,7 @@ impl Delaunay {
         let vertices: Vec<Vec<f64>> = self.vertex_iter().map(|x| x.to_vec()).collect();
         self.triangle_iter().fold(0., |s, t| {
             let (a, b, c) = (&vertices[t[0]], &vertices[t[1]], &vertices[t[2]]);
-            s + 0.5 * ((a[0] - c[0]) * (b[1] - a[1]) - (a[0] - b[0]) * (c[1] - a[0])).abs()
+            s + 0.5 * ((a[0] - c[0]) * (b[1] - a[1]) - (a[0] - b[0]) * (c[1] - a[1])).abs()
         })
     }
     /// Returns true if a point `[x,y]` is inside the triangle given by its index (`triangle_id`) in `triangles_iter`, otherwise returns false
